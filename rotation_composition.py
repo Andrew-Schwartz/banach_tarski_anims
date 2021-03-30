be6f173a996d71b8d3e067602509d3242de5d2b5
Andrew Schwartz <andrewschwartz59@gmail.com>
2021-03-30 15:55:39 -0400

    beautiful rotation

--- rotation_composition.py
+++ rotation_composition.py
@@ -1,6 +1,6 @@
 import functools
 
-import numpy.linalg as LA
+import numpy.linalg as npla
 from manim import *
 from numpy import tan, arctan
 
@@ -10,45 +10,11 @@ def pt(x, y, z):
 
 
 def unit(array: np.ndarray) -> np.ndarray:
-    norm = LA.norm(array)
+    norm = npla.norm(array)
     return array / norm
 
 
-class Rotato(ThreeDScene):
-    def rotatoes(self, rotation_vectors):
-        prism = ColorPrism()
-        individual = prism.copy()
-        self.play(ShowCreation(individual))
-        for rotation in rotation_vectors:
-            norm = LA.norm(rotation)
-            rotation = rotation / norm
-            self.play(Rotate(individual, angle=norm, axis=rotation), run_time=1.2)
-        self.wait(0.5)
-
-        # a and b are `angle * unit_vector` - see
-        # https://wikimedia.org/api/rest_v1/media/math/render/svg/6beebd8e0b921524bd82b4675d58dc984250fdc0
-        def compose(a: np.ndarray, b: np.ndarray) -> np.ndarray:
-            alpha, beta = LA.norm(a), LA.norm(b)
-            a, b = a / alpha, b / beta
-            numerator = tan(beta / 2) * b + tan(alpha / 2) * a + tan(beta / 2) * tan(alpha / 2) * np.cross(b, a)
-            denominator = 1 - tan(beta / 2) * tan(alpha / 2) * np.dot(b, a)
-            rhs = numerator / denominator
-            angle = 2 * arctan(LA.norm(rhs))
-            return angle * unit(rhs)
-
-        final_rotation = functools.reduce(compose, rotation_vectors)
-        composed = prism.copy()
-        self.play(Transform(individual, individual.copy().set_opacity(0.3)), ShowCreation(composed))
-        axis = Line3D(-final_rotation * 3, final_rotation * 3, thickness=0.005, color=RED)
-        self.play(ShowCreation(axis))
-        shift = final_rotation / max([abs(x) for x in final_rotation])
-        marker1 = Sphere(radius=0.08, color=GREEN).shift(shift)
-        marker2 = Sphere(radius=0.08, color=GREEN).shift(-shift)
-        self.play(ShowCreation(marker1), ShowCreation(marker2))
-        self.play(Rotate(composed, angle=LA.norm(final_rotation), axis=final_rotation), run_time=2)
-        self.wait(1)
-
-
+# noinspection PyAbstractClass
 class ColorPrism(VGroup):
     colors = [
         "#FFD500",
@@ -89,14 +55,53 @@ class ColorPrism(VGroup):
             self.rescale_to_fit(value, dim, stretch=True)
 
 
+class Rotato(ThreeDScene):
+    def rotatoes(self, prism: ColorPrism, rotation_vectors):
+        individual = prism.copy()
+        self.play(ShowCreation(individual))
+        for rotation in rotation_vectors:
+            norm = npla.norm(rotation)
+            rotation = rotation / norm
+            self.play(Rotate(individual, angle=norm, axis=rotation), run_time=1.2)
+        self.wait(0.5)
+
+        # a and b vectors that encode directionality and angle, they are given by `angle * unit_vector`
+        # math source: https://wikimedia.org/api/rest_v1/media/math/render/svg/6beebd8e0b921524bd82b4675d58dc984250fdc0
+        def compose(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+            alpha, beta = npla.norm(a), npla.norm(b)
+            a, b = a / alpha, b / beta
+            numerator = tan(beta / 2) * b + tan(alpha / 2) * a + tan(beta / 2) * tan(alpha / 2) * np.cross(b, a)
+            denominator = 1 - tan(beta / 2) * tan(alpha / 2) * np.dot(b, a)
+            rhs = numerator / denominator
+            angle = 2 * arctan(npla.norm(rhs))
+            return angle * unit(rhs)
+
+        final_rotation = functools.reduce(compose, rotation_vectors)
+        composed = prism.copy()
+        self.play(Transform(individual, individual.copy().set_opacity(0.25)), ShowCreation(composed))
+        axis = Line3D(-final_rotation * 3, final_rotation * 3, thickness=0.005, color=RED)
+        self.play(ShowCreation(axis))
+        shift = final_rotation / max([abs(x) for x in final_rotation])
+        shift2 = final_rotation / min([abs(x) for x in final_rotation])
+        print(f"final_rotation = {final_rotation}")
+        print(f"shift = {shift}")
+        print(f"shift2 = {shift2}")
+        marker1 = Sphere(radius=0.08, color=GREEN).shift(shift)
+        marker2 = Sphere(radius=0.08, color=GREEN).shift(-shift)
+        self.play(ShowCreation(marker1), ShowCreation(marker2))
+        self.play(Rotate(composed, angle=npla.norm(final_rotation), axis=final_rotation), run_time=2)
+        self.wait(1)
+
+
 class Composition2(Rotato):
     def construct(self):
         self.add(ThreeDAxes())
         self.set_camera_orientation(phi=75 * DEGREES, theta=-45 * DEGREES)
+        self.begin_ambient_camera_rotation(rate=0.07)
         angle = PI / 2
-        self.rotatoes([
+        self.rotatoes(ColorPrism(dimensions=[2, 2, 2]), [
             angle * OUT,
-            angle * RIGHT
+            angle * DOWN,
         ])
 
 
@@ -105,7 +110,7 @@ class Composition3(Rotato):
         self.add(ThreeDAxes())
         self.set_camera_orientation(phi=75 * DEGREES, theta=-45 * DEGREES)
         self.begin_ambient_camera_rotation(rate=0.07)
-        self.rotatoes([
+        self.rotatoes(ColorPrism(), [
             PI / 2 * RIGHT,
             PI / 2 * UP,
             PI / 5 * OUT
